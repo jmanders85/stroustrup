82d9b96259302dcba089b6463a5cb92c7e47d8f1
jmla <jmanders85@gmail.com>
2017-12-28 16:34:36 -0600

    Hello world and function declaration

---
+++ ch2/triple.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Baby's first function declaration
+int triple(int x)
+{
+  return 3 * x;
+}
+
+int main() {
+  cout << "4 tripled is " << triple(4) << endl;
+}
